e2af5401c941196dff0fcaf77a1cc9e44dd8f201
Guillaume Godet-Bar <guillaume.godetbar@gmail.com>
2009-03-22 16:15:07 +0000

    -	Gestion de la boîte de sélection pour l'ajout de liens hypertextes dans les images -	Ajout de traductions (utilisées dans le back-end) -	Ajout de quelques patrons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -26,6 +26,11 @@ Event.observe(window, 'load', function(){
 	zindex: '10'
 	}) ;
 	$('plus_sign').observe('click', function(){
+		rect_w = 180 ;
+		rect_h = 100 ;
+		rect_x = width / 2 - rect_w / 2;
+		rect_y = height / 2 - rect_h / 2;
+		
 		
 		$('plus_sign').setStyle({
 			visibility: 'hidden'
@@ -42,9 +47,9 @@ Event.observe(window, 'load', function(){
 		
 		
 		$('overlay').setStyle({
-			position: 'absolute',
-			top: image.cumulativeOffset().top + 'px',
-			left: image.cumulativeOffset().left + 'px',
+			position: 'relative',
+			top: '0px',
+			left: '0px',
 			width : width + 'px',
 			height : height + 'px',
 			background : '#000000',
@@ -55,8 +60,8 @@ Event.observe(window, 'load', function(){
 
 		$('selection').setStyle({
 			position: 'absolute',
-			top: image.cumulativeOffset().top + rect_y + 'px',
-			left: image.cumulativeOffset().left + rect_x + 'px',
+			top: rect_y + 'px',
+			left: rect_x + 'px',
 			width: rect_w + 'px',
 			height: rect_h + 'px',
 			background: '#dd0000',
@@ -88,80 +93,88 @@ Event.observe(window, 'load', function(){
 			left: rect_w - 10 + 'px',
 			visibility: 'visible'
 		});
-
 	
-		new Draggable($('selection')) ;
-		var old_sel_x = $('selection').cumulativeOffset().left ;
-		var old_sel_y = $('selection').cumulativeOffset().top ;
-		var old_sel_w = rect_w ;
-		var old_sel_h = rect_h ;
+		/* TODO  Passer les données ci-dessous en fonction ? */
+		var select_draggable = new Draggable($('selection') , {onDrag : function(){
+			old_sel_x = rect_x ;
+			old_sel_y = rect_y ;
+			old_sel_w = rect_w ;
+			old_sel_h = rect_h ;
+			rect_x = $('selection').positionedOffset().left ;
+			rect_y = $('selection').positionedOffset().top ;
+		}}) ;
 
-		new Draggable($('tl_rect'), { onDrag : function(){
-			rect_x = $('tl_rect').cumulativeOffset().left ;
-			rect_y = $('tl_rect').cumulativeOffset().top ;
-			rect_w = old_sel_x - rect_x + old_sel_w ;
-			rect_h = old_sel_y - rect_y + old_sel_h ;
+
+		var tl_rect_draggable = new Draggable($('tl_rect'), { onDrag : function(){
+			old_sel_x = rect_x ;
+			old_sel_y = rect_y ;
+			old_sel_w = rect_w ;
+			old_sel_h = rect_h ;
+			
+			rect_x = $('tl_rect').positionedOffset().left + $('selection').positionedOffset().left ;
+			rect_y = $('tl_rect').positionedOffset().top + $('selection').positionedOffset().top;		
+			rect_w = calcWidth(rect_x, old_sel_x, old_sel_w) ;
+			rect_h = calcHeight(rect_y, old_sel_y, old_sel_h) ;
+			
+			// alert('x : ' + rect_x + ' old_x : ' + old_sel_x + ' new w : ' + rect_w + ', old w : ' + old_sel_w) ;
+			// old_sel_x - rect_x + old_sel_w ;
+			// rect_h = old_sel_y - rect_y + old_sel_h ;
+
+			$('tr_rect').setStyle({
+				top: '0px',
+				left: rect_w - 10 + 'px'
+			});
+			$('br_rect').setStyle({
+				top: rect_h - 10 + 'px',
+				left: rect_w - 10 + 'px'
+			});
+			$('bl_rect').setStyle({
+				top: rect_h - 10 + 'px',
+				left: '0px'
+			});
+			
 			$('selection').setStyle({
 				top: rect_y + 'px',
 				left: rect_x + 'px',
 				width: rect_w + 'px',
 				height: rect_h + 'px'
 			});
-			$('tr_rect').setStyle({
-				position: 'absolute',
-				top: '0px',
-				left: rect_w - 10 + 'px',
-				visibility: 'visible'
-			});
-			$('br_rect').setStyle({
-				position: 'absolute',
-				top: rect_h - 10 + 'px',
-				left: rect_w - 10 + 'px',
-				visibility: 'visible'
-			});
-			$('bl_rect').setStyle({
-				position: 'absolute',
-				top: rect_h - 10 + 'px',
-				left: '0px',
-				visibility: 'visible'
-			});
-			old_sel_x = rect_x ;
-			old_sel_y = rect_y ;
-			old_sel_w = rect_w ;
-			old_sel_h = rect_h ;
 		}}) ;
 		
 		
-		new Draggable($('br_rect'), { onDrag : function(){
+		var br_rect_draggable = new Draggable($('br_rect'), { onDrag : function(){
+			old_sel_x = rect_x ;
+			old_sel_y = rect_y ;
+			old_sel_w = rect_w ;
+			old_sel_h = rect_h ;
+			
 			rect_x = old_sel_x ;
 			rect_y = old_sel_y ;
-			rect_w = $('br_rect').cumulativeOffset().left - old_sel_x + 10 ;
-			rect_h = $('br_rect').cumulativeOffset().top - old_sel_y + 10 ;
+			rect_w = $('br_rect').positionedOffset().left + $('selection').positionedOffset().left - old_sel_x + 10 ;
+			rect_h = $('br_rect').positionedOffset().top + $('selection').positionedOffset().top - old_sel_y + 10 ;
+			
+			$('tr_rect').setStyle({
+				top: '0px',
+				left: rect_w - 10 + 'px'
+			});
+			$('bl_rect').setStyle({
+				top: rect_h - 10 + 'px',
+				left: '0px'
+			});
+
 			$('selection').setStyle({
 				top: rect_y + 'px',
 				left: rect_x + 'px',
 				width: rect_w + 'px',
 				height: rect_h + 'px'
 			});
-			$('tr_rect').setStyle({
-				position: 'absolute',
-				top: '0px',
-				left: rect_w - 10 + 'px',
-				visibility: 'visible'
-			});
-			$('bl_rect').setStyle({
-				position: 'absolute',
-				top: rect_h - 10 + 'px',
-				left: '0px',
-				visibility: 'visible'
-			});
-			old_sel_x = rect_x ;
-			old_sel_y = rect_y ;
-			old_sel_w = rect_w ;
-			old_sel_h = rect_h ;
 		}}) ;
 		
 		$('validate_area').observe('click', function(){
+			select_draggable.destroy() ;
+			tl_rect_draggable.destroy() ;
+			br_rect_draggable.destroy() ;
+			
 			$('validate_area').setStyle({
 				visibility: 'hidden'
 			});
@@ -195,7 +208,7 @@ Event.observe(window, 'load', function(){
 			}) ;
 			
 			var url = '/process_patterns/choose_pattern/' + current_pattern_id ;
-			var param = {x: rect_x - image.cumulativeOffset().left,	y: rect_y - image.cumulativeOffset().top,	w: rect_w, h: rect_h} ;
+			var param = {x: rect_x,	y: rect_y,	w: rect_w, h: rect_h} ;
 			// alert('Should display pattern choice' + rect_w) ;
 			$('pattern_choice').setStyle({
 				visibility: 'visible',
@@ -212,11 +225,24 @@ Event.observe(window, 'load', function(){
  });
 }
 
+function calcWidth(new_x, old_x, old_w) {
+	tmp_x = old_x - new_x ;
+	// if (tmp_x < 0) {
+	// 	tmp_x = - tmp_x ;
+	// }
+	
+	return tmp_x + old_w ;
+}
+
+function calcHeight(new_y, old_y, old_h) {
+	return calcWidth(new_y, old_y, old_h) ;
+}
+
 function registerArea(current_pattern_id, pattern_id, x, y, w, h) {
 	
 	
 	var new_dd = new Element('dd', {id: "dd_" + pattern_id}) ;
-	var new_link = new Element('a', {id: "link_" + pattern_id, href: "../" + pattern_id}) ;
+	var new_link = new Element('a', {id: "link_" + pattern_id}) ;
 	new_dd.appendChild(new_link) ;
 	new_dd.setStyle({
 		left: x + 'px',
