2a78fe88c8c8ebc14f9f6171997b305cf26685e5
Guillaume Godet-Bar <guillaume.godetbar@gmail.com>
2009-06-01 11:50:07 +0200

    Config mistakes

--- config/environment.rb
+++ config/environment.rb
@@ -28,8 +28,8 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
-  config.gem "attachment_fu", :source  => "git://github.com/technoweenie/attachment_fu.git"
-  config.gem "cucumber", :source  => "git://github.com/aslakhellesoy/cucumber.git"
+  config.gem "attachment_fu"
+  config.gem "cucumber"
   
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
