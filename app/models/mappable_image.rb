c01d9f6111169458d4d0c0d785a70fcbb21fc3c7
unknown <Administrateur@.(none)>
2010-01-06 18:41:13 +0100

    Temporarily deactivated attachment_fu

--- app/models/mappable_image.rb
+++ app/models/mappable_image.rb
@@ -3,25 +3,25 @@ class MappableImage < ActiveRecord::Base
   has_many :image_associations
   belongs_to  :pattern_system
     
-  has_attachment  :storage => :file_system, 
-                  :max_size => 1.megabytes,
-                  :path_prefix  => "public/images",
-                  :resize_to  => "800x",
-                  :thumbnails => { :thumb => '80x80>', :tiny => '40x40>' },
-                  :processor => :MiniMagick # attachment_fu looks in this order: ImageScience, Rmagick, MiniMagick
-
-  validates_as_attachment
-  
-  # Redefines the algorithm for computing the path for each stored file.
-  # Now images are stored in a parent folder whose name corresponds to the short_name field of its parent pattern system.
-  def full_filename(thumbnail = nil)
-    file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
-    File.join(RAILS_ROOT, file_system_path, self.pattern_system.short_name, *partitioned_path(thumbnail_name_for(thumbnail)))
-    # if thumbnail.blank? && self.thumbnail.blank?
-    #   File.join(RAILS_ROOT, file_system_path, 'fullsize', *partitioned_path(thumbnail_name_for(thumbnail)))
-    # else
-    #   File.join(RAILS_ROOT, file_system_path, 'thumb', *partitioned_path(thumbnail_name_for(thumbnail)))
-    # end
-  end
+##  has_attachment  :storage => :file_system, 
+##                  :max_size => 1.megabytes,
+##                  :path_prefix  => "public/images",
+##                  :resize_to  => "800x",
+##                  :thumbnails => { :thumb => '80x80>', :tiny => '40x40>' },
+##                  :processor => :MiniMagick # attachment_fu looks in this order: ImageScience, Rmagick, MiniMagick
+##
+##  validates_as_attachment
+##  
+##  # Redefines the algorithm for computing the path for each stored file.
+##  # Now images are stored in a parent folder whose name corresponds to the short_name field of its parent pattern system.
+##  def full_filename(thumbnail = nil)
+##    file_system_path = (thumbnail ? thumbnail_class : self).attachment_options[:path_prefix].to_s
+##    File.join(RAILS_ROOT, file_system_path, self.pattern_system.short_name, *partitioned_path(thumbnail_name_for(thumbnail)))
+##    # if thumbnail.blank? && self.thumbnail.blank?
+##    #   File.join(RAILS_ROOT, file_system_path, 'fullsize', *partitioned_path(thumbnail_name_for(thumbnail)))
+##    # else
+##    #   File.join(RAILS_ROOT, file_system_path, 'thumb', *partitioned_path(thumbnail_name_for(thumbnail)))
+##    # end
+##  end
   
 end
--- config/environment.rb
+++ config/environment.rb
@@ -5,7 +5,7 @@
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.3' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -28,7 +28,6 @@ Rails::Initializer.run do |config|
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
 
-  config.gem "attachment-fu"
   config.gem "sqlite3-ruby", :lib => "sqlite3"
   config.gem "haml"
   
