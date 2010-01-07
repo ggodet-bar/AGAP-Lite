63c347829e97cfbe5045a7c245f68b5fb3f120aa
unknown <Administrateur@.(none)>
2010-01-07 12:47:19 +0100

    Switched development version to MySQL, for better handling of UTF8 encodings

--- config/environment.rb
+++ config/environment.rb
@@ -13,6 +13,7 @@ require File.join(File.dirname(__FILE__), 'boot')
 require File.join(File.dirname(__FILE__), 'agap_config')
 
 Rails::Initializer.run do |config|
+
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
