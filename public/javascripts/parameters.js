7d5eebdeb9463a3d5c0d28ec9fbe95a3cf217693
unknown <Administrateur@.(none)>
2010-01-09 16:50:05 +0100

    Added a more popupsy look and feel to the parameter window and fixed compatibility issues with IE

--- public/javascripts/parameters.js
+++ public/javascripts/parameters.js
@@ -1,15 +1,27 @@
-function manage_parameters(enable_text, disable_text) {
+var Popup = {
+	manage_parameters : function(enable_text, disable_text) {
+document.body.style.overflow = "hidden" ;
+  window.onscroll=function() { window.scrollTo(0,0) }
+  var body_size = null ;
+  if (document.documentElement) {
+   body_size = {	width : document.documentElement.clientWidth,
+		  	height : document.documentElement.clientHeight};
+  } else {
+	  body_size = document.body.getDimensions() ;
+  } 
+  var splash = new Element('div', {id : 'splash', style: 'position: fixed; left: -10px; top: -10px; width: ' + body_size.width * 1.1 + 'px;height: ' + body_size.height * 1.1 + 'px; background-color: #000; z-index: 1000' });
+  splash.setOpacity(0.5) ;
+  Element.insert(document.body,{'top' : splash}) ;
   if (Cookie.get('noob_mode') == null) {
     Cookie.set('noob_mode', 'true') ;
   }
  if (Cookie.get('noob_mode') == 'true') {
-   console.log("entering noob mode") ;
    $('params_caution_text').innerHTML = disable_text ;
    $('param_front_button').setStyle({left: '52px'});
    Droppables.add("off_position", {
       onDrop : function(draggable){
          $('param_front_button').setStyle({left: '186px'}) ;
-         Effect.Fade('params') ;
+	 Popup.close_parameter_window() ; 
          Droppables.remove('off_position') ;
          Cookie.set('noob_mode', 'false') ;
          for (var i = 0, element ; element = $$('.helper_info')[i]; i++) {
@@ -21,14 +33,13 @@ function manage_parameters(enable_text, disable_text) {
       }
    });
   } else {
-   console.log("entering expert mode") ;
    $('params_caution_text').innerHTML = enable_text ;
    $('param_front_button').setStyle({left: '186px'});
    Droppables.add("on_position", {
       onDrop : function(draggable){
          $('param_front_button').setStyle({left: '52px'}) ;
-         Effect.Fade('params') ;
-         Droppables.remove('on_position') ;
+	 Popup.close_parameter_window() ; 
+	 Droppables.remove('on_position') ;
          Cookie.set('noob_mode', 'true') ;
          for (var i = 0, element ; element = $$('.question_mark')[i]; i++) {
             element.setStyle({display: 'inline'}) ;
@@ -48,4 +59,14 @@ function manage_parameters(enable_text, disable_text) {
         }
   )
 
+	},
+
+	close_parameter_window : function() {
+		Effect.Fade('params', {afterFinish : function() {
+	 		document.body.setStyle({overflow: 'visible'}) ;
+	 		$('splash').remove() ;
+	 		window.onscroll = null ; 
+	 }});
+	}
 }
+
