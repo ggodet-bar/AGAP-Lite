41c8d66e61b9800abee9532fde7b70780c3bc631
Guillaume Godet-Bar <guillaume.godetbar@gmail.com>
2010-01-27 17:47:51 +0100

    Extracted the popup javascript from the parameter management script and added a listener for esc keys when a popup is displayed for easier exits

--- public/javascripts/parameters.js
+++ public/javascripts/parameters.js
@@ -1,22 +1,5 @@
-var Popup = {
+var Parameters = {
 	manage_parameters : function(enable_text, disable_text) {
-		document.body.style.overflow = "hidden" ;
-  	window.onscroll=function() { window.scrollTo(0,0) }
-  	var body_size = null ;
-  	if (document.documentElement) {
-  	 body_size = {	width : document.documentElement.clientWidth,
-  	      	  	height : document.documentElement.clientHeight};
-  	} else {
-  	        body_size = document.body.getDimensions() ;
-  	} 
-  	var splash = new Element('div', {id : 'splash', style: 'position: fixed; left: -10px; top: -10px; width: ' + body_size.width * 1.1 + 'px;height: ' + body_size.height * 1.1 + 'px; background-color: #000; z-index: 1000' });
-  	splash.setOpacity(0.5) ;
-  	Element.insert(document.body,{'top' : splash}) ;
-
-
-///////////////////////////////////////
-
-
   	if (Cookie.get('noob_mode') == null) {
   	  Cookie.set('noob_mode', 'true') ;
   	}
@@ -26,7 +9,7 @@ var Popup = {
   	 Droppables.add("off_position", {
   	    onDrop : function(draggable){
   	       $('param_front_button').setStyle({left: '186px'}) ;
-  	       Popup.close_parameter_window() ; 
+  	       Popup.close('params') ; 
   	       Droppables.remove('off_position') ;
   	       Cookie.set('noob_mode', 'false') ;
   	       for (var i = 0, element ; element = $$('.helper_info')[i]; i++) {
@@ -43,7 +26,7 @@ var Popup = {
   	 Droppables.add("on_position", {
   	    onDrop : function(draggable){
   	       $('param_front_button').setStyle({left: '52px'}) ;
-  	       Popup.close_parameter_window() ; 
+  	       Popup.close('params') ; 
   	       Droppables.remove('on_position') ;
   	       Cookie.set('noob_mode', 'true') ;
   	       for (var i = 0, element ; element = $$('.question_mark')[i]; i++) {
@@ -63,15 +46,6 @@ var Popup = {
   	      }
   	    }
   	) ; 
-///////////////////////////////////////////
-	},
-
-	close_parameter_window : function() {
-		Effect.Fade('params', {afterFinish : function() {
-	 		document.body.style.overflow= 'auto' ;
-	 		$('splash').remove() ;
-	 		window.onscroll = null ; 
-	 }});
 	}
 }
 
---
+++ public/javascripts/popup.js
@@ -0,0 +1,61 @@
+var Popup = {
+        func_name: null,
+        func_args: null,
+
+        // Arguments should be passed as an array
+        install_callback: function(func, args) {
+          if (func) {
+            console.log("The arguments are " + args) ;
+            Popup.func_name= func ;
+            Popup.func_args = args ;
+          }
+        },
+
+        toggle: function(popup_id) {
+          if ($('splash_137')) {
+            Popup.close(popup_id) ;
+          } else {
+            Popup.open(popup_id) ;
+          }
+        },
+
+	open : function(popup_id) {
+		document.body.style.overflow = "hidden" ;
+  	window.onscroll=function() { window.scrollTo(0,0) }
+  	var body_size = null ;
+  	if (document.documentElement) {
+  	 body_size = {	width : document.documentElement.clientWidth,
+  	      	  	height : document.documentElement.clientHeight};
+  	} else {
+  	        body_size = document.body.getDimensions() ;
+  	} 
+  	var splash = new Element('div', {id : 'splash_137', style: 'position: fixed; left: -10px; top: -10px; width: ' + body_size.width * 1.1 + 'px;height: ' + body_size.height * 1.1 + 'px; background-color: #000; z-index: 1000' });
+  	splash.setOpacity(0.5) ;
+  	Element.insert(document.body,{'top' : splash}) ;
+
+        // Call the function that finalizes the setup of
+        // the system
+        Popup.func_name.apply(this, Popup.func_args) ;
+
+        // Add a key observer for exiting the popup
+        document.observe('keydown', function(event) {
+          document.stopObserving('keydown', this) ;  
+          Popup.close(popup_id) ;
+           
+        }) ;
+
+        // The popup should be appearing
+        $(popup_id).appear() ;
+	},
+
+	close : function(popup_id) {
+		Effect.Fade(popup_id, {afterFinish : function() {
+	 		document.body.style.overflow= 'auto' ;
+	 		if ($('splash_137')) {
+                          $('splash_137').remove() ;
+                        }
+	 		window.onscroll = null ; 
+	 }});
+	}
+}
+
