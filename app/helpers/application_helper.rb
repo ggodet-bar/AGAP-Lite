a00c6c1608aec6a61de585aff3a2a1ac731f4888
Guillaume Godet-Bar <guillaume.godetbar@gmail.com>
2010-03-01 22:17:28 +0100

    Factorized the tabs code into a partial. Fixed some firefox rendering bugs.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
  
+
   def shortify(a_string)
     if !a_string.blank? && a_string.length > Agap::Config.header_link_max_size
       a_string.slice(0..Agap::Config.header_link_max_size - 1) + '...'
