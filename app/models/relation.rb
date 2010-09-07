c661ba497b4473ad0e7370dc06ac5b3a0225edf1
Guillaume Godet-Bar <guillaume.godetbar@gmail.com>
2010-09-07 16:20:36 +0200

    Cleaned up the Relation model.

--- app/models/relation.rb
+++ app/models/relation.rb
@@ -5,9 +5,9 @@ class Relation < ActiveRecord::Base
 
   after_create do |relation|
     if relation_descriptor.is_reflexive &&
-      Relation.where(:source_pattern_id => target_pattern_id)
-              .where(:target_pattern_id => source_pattern_id)
-              .where(:relation_descriptor_id => relation_descriptor_id)
+      Relation.where(:source_pattern_id => target_pattern_id) \
+              .where(:target_pattern_id => source_pattern_id) \
+              .where(:relation_descriptor_id => relation_descriptor_id) \
               .empty?
       puts "Create new relation!"
       Relation.create({
@@ -20,8 +20,8 @@ class Relation < ActiveRecord::Base
 
   after_destroy do |relation|
     if relation_descriptor.is_reflexive
-      reflexive_relation = Relation.where(:source_pattern_id => target_pattern_id)
-                                   .where(:target_pattern_id => source_pattern_id)
+      reflexive_relation = Relation.where(:source_pattern_id => target_pattern_id) \
+                                   .where(:target_pattern_id => source_pattern_id) \
                                    .where(:relation_descriptor_id => relation_descriptor_id)
       if reflexive_relation.present?
         reflexive_relation.map(&:destroy)
@@ -29,11 +29,4 @@ class Relation < ActiveRecord::Base
     end
   end
 
-  def target_pattern_name
-    target_pattern.present? ? target_pattern.name : ""
-  end
-
-  def target_pattern_name=(*args)
-
-  end
 end
